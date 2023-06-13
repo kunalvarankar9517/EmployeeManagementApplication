778ad5d0e327d977293393dc1e40d04bd418f2b9
unknown <kunalvaranakar@gmail.com>
2023-06-13 18:18:58 +0530

    springbootcrud

--- src/main/java/com/cdac/repositories/EmployeeRepository.java
+++ src/main/java/com/cdac/repositories/EmployeeRepository.java
@@ -1,9 +1,9 @@
-//package com.cdac.repositories;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.cdac.entities.Employee;
-//
-//public interface EmployeeRepository extends JpaRepository<Employee, Integer>  {
-//
-//}
+package com.cdac.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cdac.entities.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>  {
+
+}
--- src/main/java/com/cdac/service/EmployeeService.java
+++ src/main/java/com/cdac/service/EmployeeService.java
@@ -1,43 +1,43 @@
-//package com.cdac.service;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import com.cdac.entities.Employee;
-//import com.cdac.repositories.EmployeeRepository;
-//
-//@Service
-//public class EmployeeService {
-//	@Autowired
-//	private EmployeeRepository employeeRepo;
-//
-//	// save employee
-//	public Integer saveEmployee(Employee employee) {
-//		Employee emp = employeeRepo.save(employee);
-//		return emp.getEmpId();
-//	}
-//
-//	// show all employees
-//	public List<Employee> getAllEmployee() {
-//		List<Employee> employee = employeeRepo.findAll();
-//		return employee;
-//	}
-//	// delete employee by id
-//	public void deleteEmployeeById(Integer id) {
-//		employeeRepo.deleteById(id);
-//	}
-//	
-//	// get one employee using id
-//	public Employee getOneEmployee(Integer id) {
-//		return employeeRepo.findById(id).get();
-//	}
-//
-//	// update employee
-//	public Integer updateEmployee(Employee employee) {
-//		Employee e = employeeRepo.save(employee);
-//		Integer empId = e.getEmpId();
-//		return empId;
-//	}
-//}
+package com.cdac.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cdac.entities.Employee;
+import com.cdac.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	@Autowired
+	private EmployeeRepository employeeRepo;
+
+	// save employee
+	public Integer saveEmployee(Employee employee) {
+		Employee emp = employeeRepo.save(employee);
+		return emp.getEmpId();
+	}
+
+	// show all employees
+	public List<Employee> getAllEmployee() {
+		List<Employee> employee = employeeRepo.findAll();
+		return employee;
+	}
+	// delete employee by id
+	public void deleteEmployeeById(Integer id) {
+		employeeRepo.deleteById(id);
+	}
+	
+	// get one employee using id
+	public Employee getOneEmployee(Integer id) {
+		return employeeRepo.findById(id).get();
+	}
+
+	// update employee
+	public Integer updateEmployee(Employee employee) {
+		Employee e = employeeRepo.save(employee);
+		Integer empId = e.getEmpId();
+		return empId;
+	}
+}
